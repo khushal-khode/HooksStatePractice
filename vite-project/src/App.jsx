dee00a6d98a3d098e51d7e31944d5343dbe5ab3a
khushal-khode <khushal.khode@atomicloops.com>
2025-04-17 16:49:34 +0530

    feat: Created Counter Functionality in App.jsx using HookState

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,19 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { hookstate, useHookstate } from '@hookstate/core';
 
+const globalState = hookstate(0)
 export const App = () => {
+  const count = useHookstate(globalState);
   return (
     <div className="body_cont">
       <div className="header">
         <h1>Practicing React HookState</h1>
       </div>
       <div className="body">
-        
+        {'Count: '}
+        <span className="count">{count.get()}</span>
       </div>
       <div className="footer">
-      <button>Increement</button>
-        <button>Decreement</button>
-        <button>Reset</button>
+        <button onClick={() => count.set(p => p + 1)}>Increment</button>
+        <button onClick={() => count.set(p => p - 1)}>Decrement</button>
+        <button onClick={() => count.set(0)}>Reset</button>
       </div>
     </div>
-  )
-}
+  );
+};
