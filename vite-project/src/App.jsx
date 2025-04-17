c13542e756d35edd5237e435bb19f4fa4be254b6
khushal-khode <khushal.khode@atomicloops.com>
2025-04-17 15:38:08 +0530

    feat: counter

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const App = () => {
+  return (
+    <div className="body_cont">
+      <div className="header">
+        <h1>Practicing React HookState</h1>
+      </div>
+      <div className="body">
+        
+      </div>
+      <div className="footer">
+      <button>Increement</button>
+        <button>Decreement</button>
+        <button>Reset</button>
+      </div>
+    </div>
+  )
+}
